33089667b7b6217ca4dcc3c74a754451752924dd
Gabi <gabriel_marian22@yahoo.com>
2021-02-26 21:06:18 +0200

    get clinics in home page

--- src/ClientUI.js/WelcomePage.js
+++ src/ClientUI.js/WelcomePage.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
+import axios from "axios";
 
 export default function WelcomePage() {
+const [clinics, setclinics] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/clinics").then((res) => setclinics(res.data));
+  }, []);
+
   return (
     <React.Fragment>
       <Navbar />
       Welcome
+      {clinics.map((c) => <p>{c.clinicName}</p>)}
     </React.Fragment>
   );
 }
