391c609dabad3ecc4ab1a11f54466cebae4992e8
Gabi <gabriel_marian22@yahoo.com>
2021-02-26 21:21:21 +0200

    test

--- src/ClientUI.js/WelcomePage.js
+++ src/ClientUI.js/WelcomePage.js
@@ -3,17 +3,21 @@ import Navbar from "../Navbar/Navbar";
 import axios from "axios";
 
 export default function WelcomePage() {
-const [clinics, setclinics] = useState([])
+  const [clinics, setclinics] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:8080/clinics").then((res) => setclinics(res.data));
+    axios
+      .get("http://localhost:8080/clinics")
+      .then((res) => setclinics(res.data));
   }, []);
 
   return (
     <React.Fragment>
       <Navbar />
       Welcome
-      {clinics.map((c) => <p>{c.clinicName}</p>)}
+      {clinics.map((c) => (
+        <p>{c.clinicName}</p>
+      ))}
     </React.Fragment>
   );
 }
