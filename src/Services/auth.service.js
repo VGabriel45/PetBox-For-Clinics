4775710e3bf66f20200c8bd0c90e45585f47d966
VGabriel45 <56271768+VGabriel45@users.noreply.github.com>
2021-02-26 18:28:44 +0200

    test

--- src/Services/auth.service.js
+++ src/Services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://petbox-backend.herokuapp.com/api/auth/";
+const API_URL = "https://petbox-backend.herokuapp.com/";
 
 class AuthService {
   login(username, password) {
